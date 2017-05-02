98f689dc3d82917d30f90ae36bffe058cab0b0e3
Seth <seth19483@gmail.com>
2017-05-02 10:39:02 -0600

    removed accidental models and controllers, along with non index show methods

--- app/models/order.rb
+++
@@ -1,4 +0,0 @@
-class Order < ApplicationRecord
-  has_many :order_items
-  has_many :items, through: :order_items
-end
--- db/migrate/20170502151838_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[5.0]
-  def change
-    create_table :orders do |t|
-      t.string :order_number
-
-      t.timestamps
-    end
-  end
-end
