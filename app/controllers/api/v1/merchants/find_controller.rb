5cd6d1fbc8d91e11088a6234bceb5dc4cbda02ed
stovermc <stovermc@gmail.com>
2017-05-03 13:39:04 -0600

    Add find_all functionality to all endpoints
    * routes and controller actions updated
    
    * still need to add tests

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Merchants::FindController < ApplicationController
+  def index
+    render json: Merchant.where(merchant_params)
+  end
+
   def show
     render json: Merchant.find_by(merchant_params)
   end
+  
 private
   def merchant_params
     params.permit(:id,
--- app/controllers/api/v1/transactions/find_controller.rb
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::Transactions::FindController < ApplicationController
+  def index
+    render json: Transaction.where(transaction_params)
+  end
+
   def show
     render json: Transaction.find_by(transaction_params)
   end
+
 private
+
   def transaction_params
     params.permit(:id,
                   :invoice_id,
