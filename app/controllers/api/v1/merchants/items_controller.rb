7cc726941f14497d256997ce98a4cd8b2ce50f43
Seth <seth19483@gmail.com>
2017-05-03 16:22:57 -0600

    fix ordering

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    render json: Merchant.find(params[:id]).items
+    render json: Merchant.find(params[:id]).items.order(:id)
   end
 end
