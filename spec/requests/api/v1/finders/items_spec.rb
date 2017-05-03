7f82417a014a6ac440515965bda41e0d4b28a31b
stovermc <stovermc@gmail.com>
2017-05-03 11:44:36 -0600

    Add test for find item by all attributes

--- spec/requests/api/v1/finders/items_spec.rb
+++ spec/requests/api/v1/finders/items_spec.rb
@@ -89,9 +89,12 @@ describe "Items API" do
   end
 
   context "find_all" do
-    let!(:item_1) { create(:item, name: "coffee") }
-    let!(:item_2) { create(:item, name: "coffee") }
-    let!(:item_3) { create(:item) }
+    let!(:merchant_1) { create(:merchant) }
+    let!(:merchant_2) { create(:merchant) }
+    let!(:item_1) { create(:item, name: "coffee", description: "medium-bodied") }
+    let!(:item_2) { create(:item, name: "coffee", description: "medium-bodied") }
+    let!(:item_3) { create(:item, merchant_id: 1, unit_price: 3456) }
+    let!(:item_4) { create(:item, merchant_id: 1, unit_price: 3456) }
 
     it "name" do
       get '/api/v1/items/find_all', params:{ name: "coffee" }
@@ -102,6 +105,64 @@ describe "Items API" do
       items.each do |item|
         expect(item['name']).to eq "coffee"
       end
+    end
+
+    it "description" do
+      get '/api/v1/items/find_all', params:{ description: "medium-bodied" }
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items.count).to eq 2
+      items.each do |item|
+        expect(item['description']).to eq "medium-bodied"
+      end
+    end
+
+    it "merchant_id" do
+      get '/api/v1/items/find_all', params:{ merchant_id: 1 }
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items.count).to eq 2
+      items.each do |item|
+        expect(item['merchant_id']).to eq 1
+      end
+    end
+
+    it "unit_price" do
+      unit_price = "34.56"
+
+      get '/api/v1/items/find_all', params:{ unit_price: unit_price}
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items.count).to eq 2
+      items.each do |item|
+        expect(item['price']).to eq "34.56"
+      end
+    end
+
+    it "created_at" do
+      item_1.update(created_at: date)
+      item_2.update(created_at: date)
+
+      get '/api/v1/items/find_all', params:{ created_at: date }
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items.count).to eq 2
+    end
+
+    it "updated_at" do
+      item_1.update(updated_at: date)
+      item_2.update(updated_at: date)
+      item_3.update(updated_at: date)
+
+      get '/api/v1/items/find_all', params:{ updated_at: date }
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items.count).to eq 3
     end
   end
 end
