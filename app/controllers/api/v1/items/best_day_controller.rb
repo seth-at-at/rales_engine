4c24b4e0d9ecda7f24a4d847726f3dfbc2575692
stovermc <stovermc@gmail.com>
2017-05-04 22:37:32 -0600

    update best_day_controller

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Items::BestDayController < ApplicationController
   def show
-    item_best_day = Item.find(params[:id]).best_day
-    render json: item_best_day, serializer: ItemsBestDaySerializer
+    render json: Item.find(params[:id]).best_day
   end
 end
