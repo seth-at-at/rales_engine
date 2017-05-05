ceedaacd9a335fc7d321914bcda7b101c8380210
Seth <seth19483@gmail.com>
2017-05-04 20:54:00 -0600

    items ordered by most revenue complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_revenue(quantity)
+    joins(:invoices)
+    .merge(Invoice.successful)
+    .group(:id)
+    .order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
+    .first(quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,66 +4,67 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       namespace :items do
-        get "/find_all",          to: "find#index"
-        get "/find",              to: "find#show"
-        get "/random",            to: "random#show"
-        get "/:id/invoice_items", to: "invoice_items#index"
-        get "/:id/merchant",      to: "merchant#show"
+        get "/find_all",              to: "find#index"
+        get "/find",                  to: "find#show"
+        get "/random",                to: "random#show"
+        get "/:id/invoice_items",     to: "invoice_items#index"
+        get "/:id/merchant",          to: "merchant#show"
+        get "/most_revenue",          to: "most_revenue#show"
 
       end
       resources :items, only: [:index, :show]
 
       namespace :invoices do
-        get "/find_all",          to: "find#index"
-        get "/find",              to: "find#show"
-        get "/random",            to: "random#show"
-        get "/:id/merchant",      to: "merchant#show"
-        get "/:id/customer",      to: "customer#show"
-        get "/:id/items",         to: "items#index"
-        get "/:id/transactions",  to: "transactions#index"
-        get "/:id/invoice_items", to: "invoice_items#index"
+        get "/find_all",              to: "find#index"
+        get "/find",                  to: "find#show"
+        get "/random",                to: "random#show"
+        get "/:id/merchant",          to: "merchant#show"
+        get "/:id/customer",          to: "customer#show"
+        get "/:id/items",             to: "items#index"
+        get "/:id/transactions",      to: "transactions#index"
+        get "/:id/invoice_items",     to: "invoice_items#index"
       end
       resources :invoices, only: [:index, :show]
 
       namespace :invoice_items do
-        get "/find_all",          to: "find#index"
-        get "/find",              to: "find#show"
-        get "/random",            to: "random#show"
-        get "/:id/item",          to: "item#show"
-        get "/:id/invoice",       to: "invoice#show"
+        get "/find_all",              to: "find#index"
+        get "/find",                  to: "find#show"
+        get "/random",                to: "random#show"
+        get "/:id/item",              to: "item#show"
+        get "/:id/invoice",           to: "invoice#show"
       end
       resources :invoice_items, only: [:index, :show]
 
       namespace :merchants do
-        get "/find_all",          to: "find#index"
-        get "/find",              to: "find#show"
-        get "/random",            to: "random#show"
-        get "/most_revenue",      to: "most_revenue#index"
-        get "/most_items",        to: "most_items#index"
-        get "/revenue",           to: "revenue_by_date#index"
-        get "/:id/items",         to: "items#index"
-        get "/:id/invoices",      to: "invoices#index"
-        get "/:id/revenue",       to: "revenue#show"
-        get "/:id/revenue",       to: "revenue#index"
-        get "/:id/favorite_customer",       to: "favorite_customer#show"
+        get "/find_all",              to: "find#index"
+        get "/find",                  to: "find#show"
+        get "/random",                to: "random#show"
+        get "/most_revenue",          to: "most_revenue#index"
+        get "/most_items",            to: "most_items#index"
+        get "/revenue",               to: "revenue_by_date#index"
+        get "/:id/items",             to: "items#index"
+        get "/:id/invoices",          to: "invoices#index"
+        get "/:id/revenue",           to: "revenue#show"
+        get "/:id/revenue",           to: "revenue#index"
+        get "/:id/favorite_customer", to: "favorite_customer#show"
         get "/:id/customers_with_pending_invoices", to: "pending#index"
       end
       resources :merchants, only: [:index, :show]
 
       namespace :transactions do
-        get "/find_all",          to: "find#index"
-        get "/find",              to: "find#show"
-        get "/random",            to: "random#show"
-        get "/:id/invoice",       to: "invoice#show"
+        get "/find_all",              to: "find#index"
+        get "/find",                  to: "find#show"
+        get "/random",                to: "random#show"
+        get "/:id/invoice",           to: "invoice#show"
       end
       resources :transactions, only: [:index, :show]
 
       namespace :customers do
-        get "/find_all",          to: "find#index"
-        get "/find",              to: "find#show"
-        get "/random",            to: "random#show"
-        get "/:id/invoices",      to: "invoices#index"
-        get "/:id/transactions",  to: "transactions#index"
+        get "/find_all",              to: "find#index"
+        get "/find",                  to: "find#show"
+        get "/random",                to: "random#show"
+        get "/:id/invoices",          to: "invoices#index"
+        get "/:id/transactions",      to: "transactions#index"
         get "/:id/favorite_merchant", to: "favorite_merchant#show"
 
       end
